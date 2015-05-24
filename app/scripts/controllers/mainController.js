ef64b3d30dd34a00531c0af9ef5f23ef7c402339
Sun Lee <sunlee.newyork@gmail.com>
2015-05-24 18:40:40 -0400

    fixed ng-repeat - had to switch sections to array

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -5,29 +5,29 @@ angular.module('app')
 
 		$scope.scroll = 0;
 
-		$scope.sections = {
+		$scope.sections = [
 
-			intro: {
+			{
 				position: 1,
 				name: 'intro',
 				url: '/app/views/body/intro.html'
 			},
-			product: {
+			{
 				position: 2,
 				name: 'product',
 				url: '/app/views/body/product.html'
 			},
-			features: {
+			{
 				position: 3,
 				name: 'features',
 				url: '/app/views/body/features.html'
 			},
-			contact: {
+			{
 				position: 4,
 				name: 'contact',
 				url: '/app/views/body/contact.html'
 			}
 
-		}
+		]
 
 	});
