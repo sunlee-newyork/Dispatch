325cbea942abfdd165adc36ec3fbae8e9c44b425
Sun Lee <sunlee.newyork@gmail.com>
2015-07-18 18:26:20 -0400

    added CSS resolution for main hero image

--- app/scripts/controllers/pricingController.js
+++ app/scripts/controllers/pricingController.js
@@ -3,18 +3,19 @@
 angular.module('app')
 	.controller('pricingController', function ($scope) {
 
+		$scope.selected = false;
+
 		$scope.selections = {
 			numLocations: 100,
 			numDisplays: 4,			
-			corporate: 0,
+			corporate: 100,
 			franchise: 150,
 			community: 0,
 			displays: 1000,
 			design: 300,
 			installation: 600,
 			backup: 0,
-			wall: 0,
-			ceiling: 0
+
 		}
 
 		$scope.quote = {
@@ -31,10 +32,20 @@ angular.module('app')
 		$scope.$watchCollection('selections', function (newVal, oldVal) {
 			console.log("watchCollection called.");
 			console.log("newVal: ", newVal);
-			$scope.quote.perLocation.oneTime = (newVal.numDisplays * newVal.displays) + newVal.design + newVal.installation + newVal.backup + newVal.wall + newVal.ceiling;
+			$scope.quote.perLocation.oneTime = (newVal.numDisplays * newVal.displays) + newVal.design + newVal.installation + newVal.backup;
 			$scope.quote.allLocations.oneTime = $scope.quote.perLocation.oneTime * newVal.numLocations;
 			$scope.quote.perLocation.monthly = newVal.corporate + newVal.franchise + newVal.community;
 			$scope.quote.allLocations.monthly = $scope.quote.perLocation.monthly * newVal.numLocations;
 		});
 
+	})
+
+	.directive('priceHeight', function () {
+    return {
+	    restrict: 'AE',
+	    replace: true,
+	    link: function(scope, elem, attrs) {
+	    	elem.css('height', document.getElementsByClassName("price-options")[0].clientHeight + 'px');
+	    }
+	  };
 	});
